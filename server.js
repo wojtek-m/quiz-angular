cdcfb0b6750595785a3db2db9e7f9f17fad73c87
Wojtek Murawski <wojciech.murawski@gmail.com>
2015-09-30 19:34:28 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+router.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
